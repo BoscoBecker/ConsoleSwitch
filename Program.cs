ba9496fd974985171456d23f720478aebd57d0d0
Joao Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-02-10 10:20:42 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoeleSwitch
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Informe um dia da semana:");
+            int x = int.Parse(Console.ReadLine());
+            string day;
+            switch (x)
+            {
+                case 1:  day = "Domingo"; break;
+                case 2:  day = "Segunda-Feira"; break;
+                case 3:  day = "Terça-Feira"; break;
+                case 4:  day = "Quarta-Feira"; break;
+                case 5:  day = "Quinta-Feira"; break;
+                case 6:  day = "Sexta-Feira"; break;
+                case 7:  day = "Sábado"; break;                
+                default: day = "Nenhum dia  da semana"; break;
+            }
+            Console.WriteLine(day);
+        }
+    }
+}
